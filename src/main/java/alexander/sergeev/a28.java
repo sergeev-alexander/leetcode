5e1ed54ee3f8c40c0cf9ce2dcdf32c099b2c76c9
Sergeev-Alexander <alexander8168619@yandex.com>
2024-08-13 20:09:50 +0300

    added new solution to 28 task

--- src/main/java/alexander/sergeev/a28.java
+++ src/main/java/alexander/sergeev/a28.java
@@ -20,5 +20,20 @@ public class a28 {
             return haystack.indexOf(needle);
         else return -1;
     }
+
+    public static int strStr1(String haystack, String needle) {
+        if (haystack.length() < needle.length())
+            return -1;
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+            int j = 0;
+            while (haystack.charAt(i + j) == needle.charAt(j)) {
+                j++;
+                if (j == needle.length())
+                    return i;
+            }
+        }
+        return -1;
+    }
+
 }
 
