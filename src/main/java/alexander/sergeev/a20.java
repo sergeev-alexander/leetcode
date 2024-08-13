849378bbb20d979a5a907a4a8685ce6e5c454d9b
Sergeev-Alexander <alexander8168619@yandex.com>
2024-08-14 00:04:18 +0300

    add solution to 20 task

---
+++ src/main/java/alexander/sergeev/a20.java
@@ -0,0 +1,40 @@
+package alexander.sergeev;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+// 20. Valid Parentheses
+public class a20 {
+
+    /*
+    Given a string s containing just the characters '(', ')', '{', '}', '[' and ']',
+    determine if the input string is valid.
+    An input string is valid if:
+    Open brackets must be closed by the same type of brackets.
+    Open brackets must be closed in the correct order.
+    Every close bracket has a corresponding open bracket of the same type.
+     */
+
+    public static void main(String[] args) {
+
+        System.out.println(isValid("{[()]}"));
+
+    }
+
+    public static boolean isValid(String s) {
+        Deque<Character> deque = new ArrayDeque<>();
+        char[] arr = s.toCharArray();
+        for (char ch : arr) {
+            if (ch == '(')
+                deque.push(')');
+            else if (ch == '{')
+                deque.push('}');
+            else if (ch == '[')
+                deque.push(']');
+            else if (deque.isEmpty() || ch != deque.pop())
+                return false;
+        }
+        return deque.isEmpty();
+    }
+
+}
---
+++ src/main/java/alexander/sergeev/a32.java
@@ -0,0 +1,40 @@
+package alexander.sergeev;
+
+import java.util.Stack;
+
+// 32. Longest Valid Parentheses
+public class a32 {
+
+    /*
+    Given a string containing just the characters '(' and ')',
+    return the length of the longest valid (well-formed) parentheses substring.
+     */
+
+    public static void main(String[] args) {
+
+        String str = "()(()";
+
+        System.out.println(longestValidParentheses(str));
+
+    }
+
+    public static int longestValidParentheses(String s) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+        int length = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+                stack.pop();
+                if (stack.isEmpty()) {
+                    stack.push(i);
+                } else {
+                    length = Math.max(length, i - stack.peek());
+                }
+            }
+        }
+        return length;
+    }
+
+}
