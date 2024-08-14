c40d699d17ee8bfecc8644a049ef5e680a41a7b2
Sergeev-Alexander <alexander8168619@yandex.com>
2024-08-14 18:46:17 +0300

    add solution to 35 task

---
+++ src/main/java/alexander/sergeev/Temp.java
@@ -0,0 +1,5 @@
+package alexander.sergeev;
+
+public class Temp {
+
+}
---
+++ src/main/java/alexander/sergeev/a35.java
@@ -0,0 +1,49 @@
+package alexander.sergeev;
+
+import java.util.Arrays;
+
+// 35. Search Insert Position
+public class a35 {
+
+    /*
+    Given a sorted array of distinct integers and a target value,
+    return the index if the target is found.
+    If not, return the index where it would be if it were inserted in order.
+    You must write an algorithm with O(log n) runtime complexity.
+     */
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 3, 5};
+        int target = 4;
+
+        System.out.println(searchInsert(nums, target));
+
+    }
+
+    public static int searchInsertON(int[] nums, int target) {
+        int result = -1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[0] > target)
+                return 0;
+            if (nums[i] == target)
+                return i;
+            if (i == nums.length - 1 && nums[i] < target || nums[i] < target && nums[i + 1] > target)
+                return i + 1;
+        }
+        return result;
+    }
+
+    public static int searchInsert(int[] nums, int target) {
+        int middle;
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            middle = left + (right - left) / 2;
+            if (nums[middle] == target) return middle;
+            if (target < nums[middle]) right = middle - 1;
+            else left = middle + 1;
+        }
+        return left;
+    }
+}
