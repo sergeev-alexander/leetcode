6a4f1b04d01c6516d28e7f0b36283cf103048a6b
Sergeev-Alexander <alexander8168619@yandex.com>
2024-08-19 22:11:26 +0300

    add solution to brackets task

---
+++ src/main/java/alexander/sergeev/ValidBreckets.java
@@ -0,0 +1,35 @@
+package alexander.sergeev;
+
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+public class ValidBreckets {
+
+    public static void main(String[] args) {
+
+        System.out.println(isValidBrackets("[{}()]"));
+
+    }
+
+    private static boolean isValidBrackets(String input) {
+        Map<Character, Character> brackets = new HashMap<>();
+        brackets.put(')', '(');
+        brackets.put('}', '{');
+        brackets.put(']', '[');
+
+        Deque<Character> stack = new LinkedList<>();
+        for (char c : input.toCharArray()) {
+            if (brackets.containsValue(c)) {
+                stack.push(c);
+            } else if (brackets.containsKey(c)) {
+                if (stack.isEmpty() || stack.pop() != brackets.get(c)) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+
+    }
+}
