d3fb5fb404c214e52a333dd1202484f5e6c5b13d
Sergeev-Alexander <alexander8168619@yandex.com>
2024-08-20 12:28:57 +0300

    add solution to 58 task

--- src/main/java/alexander/sergeev/Temp.java
+++ src/main/java/alexander/sergeev/Temp.java
@@ -1,5 +1,11 @@
 package alexander.sergeev;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class Temp {
 
+    public static void main(String[] args) {
+    }
 }
+
---
+++ src/main/java/alexander/sergeev/a58.java
@@ -0,0 +1,24 @@
+package alexander.sergeev;
+
+// 58. Length of Last Word
+public class a58 {
+
+    /*
+    Given a string s consisting of words and spaces,
+    return the length of the last word in the string.
+    A word is a maximal substring
+    consisting of non-space characters only.
+     */
+
+    public static void main(String[] args) {
+
+        String s = "Hello World";
+
+        System.out.println(lengthOfLastWord(s));
+    }
+
+    public static int lengthOfLastWord(String s) {
+        String[] arr = s.split(" ");
+        return arr[arr.length - 1].length();
+    }
+}
