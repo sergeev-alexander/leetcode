009b320bf5deca53e7c9cd7d422bfcfd7a63964f
Alexander Sergeev <a79164220703@gmail.com>
2024-08-27 14:16:26 +0300

    add solution to Fibonacci numbers with stream

---
+++ src/main/java/alexander/sergeev/FibonacciNumbersWithStream.java
@@ -0,0 +1,14 @@
+package alexander.sergeev;
+
+import java.util.stream.Stream;
+
+public class FibonacciNumbersWithStream {
+
+    public static void main(String[] args) {
+
+        Stream.iterate(new int[]{0, 1}, n -> new int[]{n[1], n[0] + n[1]})
+                .limit(20)
+                .map(n -> n[0])
+                .forEach(System.out::println);
+    }
+}
