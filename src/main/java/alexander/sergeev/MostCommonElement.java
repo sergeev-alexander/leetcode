cf126cba0def60a7fcc1ef03b894e35e00f0997a
Alexander Sergeev <a79164220703@gmail.com>
2024-09-02 12:55:24 +0300

    add solution to most common element with stream

---
+++ src/main/java/alexander/sergeev/MostCommonElement.java
@@ -0,0 +1,19 @@
+package alexander.sergeev;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class MostCommonElement {
+
+    public static void main(String[] args) {
+
+        List<Integer> data = List.of(1, 2, 3, 4, 4, 3, 4, 6, 8, 10);
+        int result = findMostFrequentElement(data);
+        System.out.println(result);
+    }
+
+    public static int findMostFrequentElement(List<Integer> data) {
+        return Collections.max(data, Comparator.comparingInt(n -> Collections.frequency(data, n)));
+    }
+}
