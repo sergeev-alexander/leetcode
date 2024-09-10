7187c100dd1a9f27902f22ec3d9a2feb84d3712c
Alexander Sergeev <a79164220703@gmail.com>
2024-09-10 20:10:06 +0300

    add solutions to 88 task

---
+++ src/main/java/alexander/sergeev/leetcode/tasks/a88.java
@@ -0,0 +1,43 @@
+package alexander.sergeev.leetcode.tasks;
+
+import java.util.Arrays;
+
+public class a88 {
+
+    /*
+    You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n,
+    representing the number of elements in nums1 and nums2 respectively.
+    Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+    The final sorted array should not be returned by the function, but instead be stored inside the array nums1.
+    To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged,
+    and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+     */
+
+    public static void main(String[] args) {
+        int[] nums1 = {1,2,3,0,0,0};
+        int[] nums2 = {2,5,6};
+        int m = 3;
+        int n = 3;
+
+        merge(nums1, m, nums2, n);
+        System.out.println(Arrays.toString(nums1));
+    }
+
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int[] nums1Copy = new int[m];
+        for (int i = 0; i < m; i++) {
+            nums1Copy[i] = nums1[i];
+        }
+        int p1 = 0;
+        int p2 = 0;
+        for (int p = 0; p < m + n; p++) {
+            if (p2 >= n || (p1 < m && nums1Copy[p1] < nums2[p2])) {
+                nums1[p] = nums1Copy[p1++];
+            } else {
+                nums1[p] = nums2[p2++];
+            }
+        }
+    }
+}
+
+
