74784a79530abc686055ef7f6f6526b278ee649e
Alexander Sergeev <a79164220703@gmail.com>
2024-09-17 15:46:39 +0300

    add solution for 121 task

---
+++ src/main/java/alexander/sergeev/leetcode/tasks/a121.java
@@ -0,0 +1,30 @@
+package alexander.sergeev.leetcode.tasks;
+
+// 121. Best Time to Buy and Sell Stock
+public class a121 {
+
+    /*
+    You are given an array prices where prices[i] is the price of a given stock on the ith day.
+    You want to maximize your profit by choosing a single day to buy one stock
+    and choosing a different day in the future to sell that stock.
+    Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+     */
+
+    public static void main(String[] args) {
+
+        int[] prices = {7, 1, 5, 3, 6, 4};
+
+        System.out.println(maxProfit(prices));
+    }
+
+    public static int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minPrice) minPrice = prices[i];
+            else if (prices[i] - minPrice > maxProfit) maxProfit = prices[i] -
+                    minPrice;
+        }
+        return maxProfit;
+    }
+}
