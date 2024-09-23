d402f1dfcd92734ad60e5941ab3f10c7dbc6a0e5
Alexander Sergeev <a79164220703@gmail.com>
2024-09-23 14:59:46 +0300

    add a slow solution for 128 task

---
+++ src/main/java/alexander/sergeev/leetcode/tasks/a128_NO_SOLUTION.java
@@ -0,0 +1,34 @@
+package alexander.sergeev.leetcode.tasks;
+
+public class a128 {
+
+    public static void main(String[] args) {
+
+        int[] nums = {100, 4, 200, 1, 3, 2};
+
+        System.out.println(longestConsecutive_SLOW(nums));
+    }
+
+    public static int longestConsecutive_SLOW(int[] nums) {
+        int longestStreak = 0;
+        for (int num : nums) {
+            int currentNum = num;
+            int currentStreak = 1;
+            while (arrayContains(nums, currentNum + 1)) {
+                currentNum += 1;
+                currentStreak += 1;
+            }
+            longestStreak = Math.max(longestStreak, currentStreak);
+        }
+        return longestStreak;
+    }
+
+    private static boolean arrayContains(int[] arr, int num) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == num) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
