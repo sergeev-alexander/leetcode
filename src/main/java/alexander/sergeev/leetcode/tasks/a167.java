0d14ff727ba0422c66c2711326df72795fb7ddcc
Alexander Sergeev <a79164220703@gmail.com>
2024-09-26 19:56:53 +0300

    add another solution for 167 task

--- src/main/java/alexander/sergeev/leetcode/tasks/a167.java
+++ src/main/java/alexander/sergeev/leetcode/tasks/a167.java
@@ -15,10 +15,22 @@ public class a167 {
 
     public static void main(String[] args) {
 
-        int[] n = {2,3,4};
-        int target = 6;
+        int[] n = {2, 3, 4, 5, 6, 7, 8, 9};
+        int target = 11;
 
         System.out.println(Arrays.toString(twoSum(n, target)));
+    }
+
+    public static int[] twoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length - 1;
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+            if (sum == target) return new int[]{left + 1, right + 1};
+            if (sum < target) left++;
+            else right--;
+        }
+        return null;
     }
 
     public static int[] twoSum_VERY_SLOW(int[] numbers, int target) {
