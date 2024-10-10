f68aadd5496fef29b1a869b513680bdb6134138b
Alexander Sergeev <a79164220703@gmail.com>
2024-10-10 16:14:42 +0300

    add solutions to 509 task

---
+++ src/main/java/alexander/sergeev/leetcode/tasks/a509.java
@@ -0,0 +1,21 @@
+package alexander.sergeev.leetcode.tasks;
+
+// 509. Fibonacci Number
+public class a509 {
+
+    /*
+    Given n, calculate F(n).
+     */
+
+    public int fib(int n) {
+        int[] arr = new int[n + 2];
+        arr[0] = 0;
+        arr[1] = 1;
+        if (n > 1) { // case n = 0 or n = 1
+            for (int i = 2; i < n + 1; i++) {
+                arr[i] = arr[i - 2] + arr[i - 1];
+            }
+        }
+        return arr[n];
+    }
+}
