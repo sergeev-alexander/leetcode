3f6cd300a2433022388e859ac9dcbb03bb9f0e21
Alexander Sergeev <a79164220703@gmail.com>
2024-10-14 18:33:29 +0300

    add more solutions to 3131 task

--- src/main/java/alexander/sergeev/leetcode/tasks/a3131.java
+++ src/main/java/alexander/sergeev/leetcode/tasks/a3131.java
@@ -22,8 +22,22 @@ public class a3131 {
     }
 
     public static int addedInteger(int[] nums1, int[] nums2) {
+        int min1 = nums1[0];
+        int min2 = nums2[0];
+        for (int i = 1; i < nums1.length; i++) {
+            if (nums1[i] < min1) min1 = nums1[i];
+            if (nums2[i] < min2) min2 = nums2[i];
+        }
+        return min2 - min1;
+    }
+
+    public static int addedInteger_SLOW(int[] nums1, int[] nums2) {
         Arrays.sort(nums1);
         Arrays.sort(nums2);
         return nums2[0] - nums1[0];
+    }
+
+    public static int addedInteger_VERY_SLOW(int[] nums1, int[] nums2) {
+        return Arrays.stream(nums2).min().getAsInt() - Arrays.stream(nums1).min().getAsInt();
     }
 }
