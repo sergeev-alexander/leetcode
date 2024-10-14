0e794e60f5e3c5a4c1d75ee52c2fba71d65b0915
Alexander Sergeev <a79164220703@gmail.com>
2024-10-14 19:28:03 +0300

    add more solutions to 3120 task

---
+++ src/main/java/alexander/sergeev/leetcode/tasks/a3120.java
@@ -0,0 +1,25 @@
+package alexander.sergeev.leetcode.tasks;
+
+import java.util.Arrays;
+
+// 3120. Count the Number of Special Characters I
+public class a3120 {
+
+    /*
+    You are given a string word. A letter is called special if it appears both in lowercase and uppercase in word.
+    Return the number of special letters in word.
+     */
+
+    public static void main(String[] args) {
+
+        String s = "aaAbcBC";
+        System.out.println(numberOfSpecialChars(s));
+    }
+
+    public static int numberOfSpecialChars(String word) {
+        return (int) Arrays.stream(word.split(""))
+                .filter(s -> Character.isLowerCase(s.charAt(0)) && word.contains(String.valueOf(Character.toUpperCase(s.charAt(0)))))
+                .distinct()
+                .count();
+    }
+}
