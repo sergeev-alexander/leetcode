67dddae30bdc7473f3297eb99066525f55061cc6
Alexander Sergeev <a79164220703@gmail.com>
2024-10-15 13:41:52 +0300

    add solution to 2951 task

---
+++ src/main/java/alexander/sergeev/leetcode/tasks/a2951.java
@@ -0,0 +1,33 @@
+package alexander.sergeev.leetcode.tasks;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// 2951. Find the Peaks
+public class a2951 {
+
+    /*
+    You are given a 0-indexed array mountain. Your task is to find all the peaks in the mountain array.
+    Return an array that consists of indices of peaks in the given array in any order.
+    Notes:
+    A peak is defined as an element that is strictly greater than its neighboring elements.
+    The first and last elements of the array are not a peak.
+     */
+
+    public static void main(String[] args) {
+
+        int[] a = {1,4,3,8,5};
+        System.out.println(findPeaks(a));
+    }
+
+    // BEATS 100%
+    public static List<Integer> findPeaks(int[] mountain) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 1; i < mountain.length - 1; i++) {
+            if (mountain[i - 1] < mountain[i] && mountain[i] > mountain[i + 1]) {
+                list.add(i);
+            }
+        }
+        return list;
+    }
+}
