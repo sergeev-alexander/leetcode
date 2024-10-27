303361939abb5f6026220928851195b3581d2f3d
Alexander Sergeev <a79164220703@gmail.com>
2024-10-27 18:27:44 +0300

    add one more solution to 2540 task

--- src/main/java/alexander/sergeev/leetcode/tasks/a2540.java
+++ src/main/java/alexander/sergeev/leetcode/tasks/a2540.java
@@ -13,15 +13,35 @@ public class a2540 {
      */
 
     public static void main(String[] args) {
-        int[] num1 = {1,2,3,6};
-        int[] num2 = {2,3,4,5};
+        int[] num1 = {1, 2, 3, 4};
+        int[] num2 = {4, 5, 6};
         System.out.println(getCommon(num1, num2));
     }
 
-    // BEATS 15%
+    // BEATS 87%
     public static int getCommon(int[] nums1, int[] nums2) {
-        Set<Integer> set  = new HashSet<>();
-        for(int n : nums1) set.add(n);
+        int point1 = 0;
+        int point2 = 0;
+        int length = Math.max(nums1.length, nums2.length);
+        while (point1 < length && point2 < length) {
+            if (nums1[point1] == nums2[point2]) return nums1[point1];
+            if (nums1[point1] > nums2[point2]) {
+                if (point2 < nums2.length - 1) {
+                    point2++;
+                } else break;
+            } else {
+                if (point1 < nums1.length - 1) {
+                    point1++;
+                } else break;
+            }
+        }
+        return -1;
+    }
+
+    // BEATS 15%
+    public static int getCommon1(int[] nums1, int[] nums2) {
+        Set<Integer> set = new HashSet<>();
+        for (int n : nums1) set.add(n);
         int min = Integer.MAX_VALUE;
         for (int n : nums2)
             if (set.contains(n) && min > n) min = n;
