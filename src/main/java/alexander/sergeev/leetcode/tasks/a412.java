453dd59ec7959611dfa204a8dd4226cc8fafc911
Alexander Sergeev <a79164220703@gmail.com>
2024-12-02 14:32:43 +0300

    add solution to 412 task

---
+++ src/main/java/alexander/sergeev/leetcode/tasks/a412.java
@@ -0,0 +1,33 @@
+package alexander.sergeev.leetcode.tasks;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// 412. Fizz Buzz
+public class a412 {
+
+    /*
+    Given an integer n, return a string array answer (1-indexed) where:
+    answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+    answer[i] == "Fizz" if i is divisible by 3.
+    answer[i] == "Buzz" if i is divisible by 5.
+    answer[i] == i (as a string) if none of the above conditions are true.
+     */
+
+    public static void main(String[] args) {
+        int n = 15;
+        System.out.println(fizzBuzz(n));
+    }
+
+    // BEATS 99%
+    public static List<String> fizzBuzz(int n) {
+        List<String> list = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (i % 15 == 0) list.add("FizzBuzz");
+            else if (i % 5 == 0) list.add("Buzz");
+            else if (i % 3 == 0) list.add("Fizz");
+            else list.add(String.valueOf(i));
+        }
+        return list;
+    }
+}
