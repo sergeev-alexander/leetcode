edde2d07a12f46511c1eda988fad884e00d103e2
Alexander Sergeev <a79164220703@gmail.com>
2024-12-05 14:50:45 +0300

    add solutions to 485 task

---
+++ src/main/java/alexander/sergeev/leetcode/tasks/a485.java
@@ -0,0 +1,45 @@
+package alexander.sergeev.leetcode.tasks;
+
+// 485. Max Consecutive Ones
+public class a485 {
+
+    /*
+    Given a binary array nums, return the maximum number of consecutive 1's in the array.
+     */
+
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 0, 1, 1, 1};
+        System.out.println(findMaxConsecutiveOnes(arr));
+    }
+
+    // BEATS 94%
+    public static int findMaxConsecutiveOnes(int[] nums) {
+        int max = 0;
+        int count = 0;
+        for (int num : nums) {
+            if (num == 1) {
+                count++;
+            } else {
+                max = Math.max(max, count);
+                count = 0;
+            }
+        }
+        max = Math.max(max, count);
+        return max;
+    }
+
+    // BEATS 54%
+    public static int findMaxConsecutiveOnes1(int[] nums) {
+        int max = 0;
+        int count = 0;
+        for (int num : nums) {
+            if (num == 1) {
+                count++;
+                max = Math.max(max, count);
+            } else {
+                count = 0;
+            }
+        }
+        return max;
+    }
+}
