5a83c0720bc488fbd5cd9780f47c21992e91e060
Alexander Sergeev <a79164220703@gmail.com>
2024-12-23 17:30:30 +0300

    add solution to 2788 task

---
+++ src/main/java/alexander/sergeev/leetcode/tasks/a2788.java
@@ -0,0 +1,43 @@
+package alexander.sergeev.leetcode.tasks;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// 2788. Split Strings by Separator
+public class a2788 {
+
+    /*
+    Given an array of strings words and a character separator, split each string in words by separator.
+    Return an array of strings containing the new strings formed after the splits, excluding empty strings.
+    Notes
+    separator is used to determine where the split should occur, but it is not included as part of the resulting strings.
+    A split may result in more than two strings.
+    The resulting strings must maintain the same order as they were initially given.
+     */
+
+    public static void main(String[] args) {
+        List<String> words = List.of("$easy$","$problem$");
+        char sep = '$';
+        System.out.println(splitWordsBySeparator(words, sep));
+    }
+
+    // BEATS 93%
+    public static List<String> splitWordsBySeparator(List<String> words, char separator) {
+        List<String> list = new ArrayList<>();
+        for (String word : words) {
+            StringBuilder stringBuilder = new StringBuilder();
+            for (char ch : word.toCharArray()) {
+                if (ch == separator) {
+                    if (!stringBuilder.isEmpty()) {
+                        list.add(stringBuilder.toString());
+                        stringBuilder = new StringBuilder();
+                    }
+                } else stringBuilder.append(ch);
+            }
+            if (!stringBuilder.isEmpty()) {
+                list.add(stringBuilder.toString());
+            }
+        }
+        return list;
+    }
+}
